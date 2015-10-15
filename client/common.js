6f4dcf9ae365233c4169bedbaedfc298d875256b
Jayanth <jayanth.bagare@gmail.com>
2015-10-16 00:48:32 +0530

    Carousel Commit

--- client/common.js
+++ client/common.js
@@ -2,3 +2,13 @@
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
+
+Template.home.onRendered(function(){
+  $("#owl-carousel").owlCarousel({
+    autoplay:3000,
+    items:1,
+    itemsDesktop: [1199, 1],
+    itemsDesktopSmall: [979, 1],
+    loop:true
+  });
+});
